933949e2d7426929b2881632d1bee4ff0b35d89a
franklinlem <franklinlem@yahoo.com>
2023-08-19 23:48:52 -0300

    aula

--- opternario.cpp
+++ opternario.cpp
@@ -9,6 +9,14 @@ int main(){
     cin >> n2;
     nota = (n1 + n2);
     (nota >= 60) ? res="ALUNO APROVADO!" : res="ALUNO REPROVADO!";
+    //res=(nota >= 60) ? "ALUNO APROVADO!" : "ALUNO REPROVADO!";
      cout << "Situacao do Aluno: " << res << endl;
+
+     int a, b;
+     a=5;
+     cout << "Digite um valor: " << endl;
+     cin >> b;
+     (b >= 10) ? a++ : a--;
+     cout << "O valor de 'a' agora e: " << a << endl;
     return 0;
 }
